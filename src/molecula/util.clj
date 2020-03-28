3edfb0b87200421cab65415dbd5802de2aeaf3a9
sfertman <sfertman@zoho.com>
2020-03-28 13:19:57 -0400

    util docstring

--- src/molecula/util.clj
+++ src/molecula/util.clj
@@ -6,12 +6,19 @@
 ;; this should probably be in molecula.redis
 
 (defn zipseq
-  "Returns a lazy sequence of partitions of interleaved colls"
+  "Returns a lazy sequence of partitions of interleaved colls.
+  Given `c1, c2, ..., cn` of the same length returns:
+  ```
+  '(((first  c1) (first  c2) ... (first  cn))
+    ((second c1) (second c2) ... (second cn))
+    ...
+    ((last   c1) (last   c2) ... (last   cn)))
+  ```
+  If lengths are different then the returned seq will have the length of the shortest collection (like with ) return upto the length of the shortest collection and the tails of other collection discarded. There is no padding like with `partition` for example."
   [& colls]
   (partition (count colls) (apply interleave colls)))
 
-
 (defn zipvec
-  "Returns a vector of partitions of interleaved colls"
+  "Like zipseq but returns all vectors."
   [& colls]
   (vec (map vec (partition (count colls) (apply interleave colls)))))
