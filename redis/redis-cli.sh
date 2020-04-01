0e8aa9997262dfd5905d683db0d7f4df1212ad5a
sfertman <sfertman@zoho.com>
2020-03-31 21:42:00 -0400

    run redis-cli inside redis-backend

--- redis/redis-cli.sh
+++ redis/redis-cli.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-[ -z ${1} ] && CONTAINER_NAME="redis_cli" || CONTAINER_NAME="redis_cli_${1}"
-
-docker run --rm -it \
-  --name "${CONTAINER_NAME}" \
-  --network molecula_net \
-  redis:alpine redis-cli -h redis_backend
+docker-compose exec redis_backend redis-cli
